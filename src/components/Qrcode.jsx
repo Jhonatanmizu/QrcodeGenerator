fd7eadc088365cfdf0804793a15acb7bf963386a
Jhonatanmizu <natanjesuss20@gmail.com>
2021-09-16 20:27:20 -0300

    finished  Project

---
+++ src/components/Qrcode.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import QRious from 'qrious'
+
+export default function Qrcode(props) {
+  const [text, setText] = useState('')
+  function generateCode(e){
+    const qr = new QRious({
+      element: document.getElementById('qrcode'),
+      size:300
+    })
+    setText(e.target.value)
+    qr.value = text
+    console.log(qr.value);
+  
+    return qr.toDataURL()
+  }
+
+  return (
+    <div >
+      <h1>{props.title}</h1>
+      <div>
+        <input type="text" name="qrcode" 
+        placeholder="Digite o site que deseja gerar o qrcode" 
+        value={text} onChange={generateCode} />
+      </div>
+      <canvas id="qrcode"></canvas>
+    </div>
+  );
+}
